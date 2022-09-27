16e98604876ec10db27862eb0456b56dafe47312
SHAMTZEWAH <shamtw-wp19@student.tarc.edu.my>
2022-09-27 17:06:24 +0800

    alert

--- EmpApp.py
+++ EmpApp.py
@@ -121,11 +121,54 @@ def GetEmpName():
         emp_id = request.args['emp_id'] #request = page, args[''] = query string
         cursor.execute("SELECT CONCAT(fname, ' ', lname) AS name FROM employee WHERE id = (%s)", (emp_id)) #value of emp_id is from data field
         value = cursor.fetchall()
-        name = value[0]
+        for row in value:
+            print(row[0])
+            name = row[0]
+            break
         db_conn.commit()
         cursor.close()
    
     return render_template('TakeAttendance.html', emp_id=emp_id, name=name) 
+
+@app.route("/addcheckin", methods=['GET'])
+def addCheckIn():
+    emp_id = 0
+    cursor = db_conn.cursor()
+    name = ""
+    in_time = ""
+    out_time = ""
+    isExist = 0
+    msg = ""
+
+    if (request.method == 'GET') :
+        emp_id = request.args['emp_id'] #request = page, args[''] = query string
+        cursor.execute("SELECT CONCAT(fname, ' ', lname) AS name, MAX(in_time), MAX(out_time) FROM employee INNER JOIN attendance ON employee.id = attendance.emp_id WHERE id = (%s)", (emp_id)) #value of emp_id is from data field
+        value = cursor.fetchone()
+        name = value[0]
+        print(name)
+
+        in_time = value[1]
+        out_time = value[2]
+
+        #check whether the emp id exists
+        if(name != ""):
+            #check whether the last time it is check in or checkout
+            if(in_time < out_time): #if checkout then can check in
+                #insert data
+
+                return in_time
+            elif(in_time > out_time):  #else tell them that they have checked in
+                #display message box
+                isExist = 1
+            else:
+                #display message box
+                isExist = 2
+                
+        else:
+            #display message box
+            isExist = 3
+
+        return render_template('TakeAttendance.html', emp_id=emp_id, name=name, isExist=isExist) 
 
 #@@@@@@@@@@Payroll
 
