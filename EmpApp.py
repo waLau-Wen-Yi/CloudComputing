091987b69a7639d114bdc10f9857776f331e4952
SHAMTZEWAH <shamtw-wp19@student.tarc.edu.my>
2022-09-27 15:44:02 +0800

    u

--- EmpApp.py
+++ EmpApp.py
@@ -115,14 +115,15 @@ def TakeAttendance():
 def GetEmpName():
     emp_id = 0
     name_arr = []
+    cursor = db_conn.cursor()
     if (request.method == 'GET') :
         emp_id = request.args['emp_id'] #request = page, args[''] = query string
-        db_conn.cursor().execute("SELECT fname, lname FROM employee WHERE id = (%s)", (emp_id)) #value of emp_id is from data field
-        value = db_conn.cursor().fetchall()
+        cursor.execute("SELECT fname, lname FROM employee WHERE id = (%s)", (emp_id)) #value of emp_id is from data field
+        value = cursor.fetchall()
         for row in value:
             print(row[0] + " " + row[1])
             name_arr.append(row[0] + " " + row[1])
-
+        cursor.close()
     return render_template('TakeAttendance.html', emp_id=name_arr[0]) 
 
 #@@@@@@@@@@Payroll
