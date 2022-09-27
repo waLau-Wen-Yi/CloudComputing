f1346a742559403f4733a38c9cf0508d4ee7d0a6
destiny232323 <77605551+destiny232323@users.noreply.github.com>
2022-09-27 23:47:46 +0800

    Update EmpApp.py

--- EmpApp.py
+++ EmpApp.py
@@ -23,6 +23,117 @@ db_conn = connections.Connection(
 output = {}
 table = 'employee'
 
+#------------------------------------------guan tong------------------------------------------------
+@app.route("/back", methods=['GET', 'POST'])
+def back():
+    return render_template('salary.html')
+
+@app.route("/salary", methods=['GET', 'POST'])
+def salary():
+    return render_template('salary.html')
+
+@app.route("/examine", methods=['GET', 'POST'])
+def examine():
+    return render_template('examine.html')
+    
+@app.route("/update", methods=['GET', 'POST'])
+def update():
+    return render_template('update.html')
+
+@app.route("/view", methods=['GET', 'POST'])
+def view():
+    cursor = db_conn.cursor()
+    string = "Select fname from employee"
+    cursor.execute(string)
+    result1 = cursor.fetchall()
+
+    string2 = "Select lname from employee"
+    cursor.execute(string2)
+    result2 = cursor.fetchall()
+
+    string3 = "Select salary from employee"
+    cursor.execute(string3)
+    result3 = cursor.fetchall()
+
+    string4 = "Select Sum(salary) from employee"
+    cursor.execute(string4)
+    result4 = cursor.fetchone()
+
+
+    db_conn.commit()
+    cursor.close()
+
+    return render_template('view.html', result1 = result1, result2 = result2, result3 = result3, result4 = result4[0])
+
+@app.route("/search2", methods=['POST'])
+def search2():
+    emp_id = request.form['emp2_id']
+    overtime = request.form['ot']
+    penalty = request.form['penalty']
+    epf = request.form['epf']
+    cursor = db_conn.cursor()
+    
+    selectSql = "Select salary From employee Where emp_id = %s"
+    id = (emp_id)
+    cursor.execute(selectSql, id)
+    result1 = cursor.fetchone()
+
+    str(overtime)
+    str(penalty)
+    str(epf)
+    
+    total = int(result1[0]) + (int(overtime) * 10) - int(penalty)
+    final = ((100 - float(epf)) * float(total)) / 100
+
+    updateSql = "Update employee set salary = %s Where emp_id = %s"
+    money = (final)
+    id = (emp_id)
+    cursor.execute(updateSql, (money, id))
+
+    selectSql2 = "Select salary From employee Where emp_id = %s"
+    id = (emp_id)
+    cursor.execute(selectSql2, id)
+    finalSalary = cursor.fetchone()
+    db_conn.commit()
+    cursor.close()
+
+    return render_template('update.html', result = result1[0], finalSalary = finalSalary[0])
+
+@app.route("/search", methods=['POST'])
+def search():
+    emp_id = request.form['emp1_id']
+    
+    cursor = db_conn.cursor()
+    selectSql = "Select salary From employee Where emp_id = %s"
+    id = (emp_id)
+    cursor.execute(selectSql, id)
+    result2 = cursor.fetchone()
+    db_conn.commit()
+
+    cursor.close()  
+    return render_template('examine.html', result = result2[0])
+
+#-----------------------------------------------------------------------------------------------------------------
+#@app.route("/addemp", methods=['POST'])
+#def AddEmp():
+#    emp_id = request.form['emp_id']   
+ #   first_name = request.form['first_name']
+   #pri_skill = request.form['pri_skill']
+  #  location = request.form['location']
+   # position = request.form['position']
+   # emp_image_file = request.files['emp_image_file']
+
+    #insert_sql = "INSERT INTO employee (emp_id, first_name, last_name, pri_skill, location, position, salary) VALUES (%s, %s, %s, %s, %s, %s, %s)"
+    #cursor = db_conn.cursor()
+
+   # if position == 'Senior':
+    #    salary = 6000
+    #else:
+    #    salary = 3000
+    #cursor.execute(insert_sql, (emp_id, first_name, last_name, pri_skill, location, position, salary))
+    #db_conn.commit()   
+
+
 #@@@@@@@@@@General
 @app.route("/", methods=['GET', 'POST'])
 def home():
